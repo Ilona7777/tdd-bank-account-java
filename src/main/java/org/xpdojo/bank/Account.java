eb02e6bb42554caede6988244d1919ce099b3cbd
ilona7777 <ilona.memmesheimer@gmail.com>
2020-07-16 16:22:47 +0200

    test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,8 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public balance=0;
+    public int getBalance(){
+        return balance;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+
+
+        Account testAccount=new Account();
+        assertTrue (testAccount.getBalance()==0);
     }
 }
